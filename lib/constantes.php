499940ff1cc94084d9031d12518638d7759eb809
juanantoniomosquera <>
2014-03-30 23:36:07 +0200

    Engadido phpass

--- lib/constantes.php
+++ lib/constantes.php
@@ -10,8 +10,8 @@ define('RUTA_FOTOS', RAIZ . 'fotos/');
 
 // Bases de datos.
 define('BD_SERVIDOR', 'localhost');
-define('BD_NOMBRE', 'xxx');
-define('BD_USUARIO', 'xxx');
-define('BD_PASSWORD', 'xxx');
+define('BD_NOMBRE', 'forcer_Rivera_wordpress');
+define('BD_USUARIO', 'forcer_reverbas');
+define('BD_PASSWORD', 'reverbas_2014');
 
 ?>
